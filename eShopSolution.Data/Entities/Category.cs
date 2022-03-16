bbadc42a6734709f64e2a31aaa84f5b408013d09
quanndm <minhquan1852@gmail.com>
2022-03-16 16:53:04 +0700

    add entry database

---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id{set; get; }
+        public int Sortorder { set; get; }
+        public bool ISShowonHome{set; get;}
+        public int? ParentId { set; get; }
+        public Status Status{set; get; }
+    }
+}
---
+++ eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    //configure entity by attribute
+    //[Table("Products")]
+    public class Product
+    {
+
+        public int Id { set; get; }
+
+        public decimal Price { set; get; }
+
+        public decimal OriginalPrice { set; get; }
+
+        public int Stock{set; get; }
+        //[Required]
+        public int ViewCount{set; get; }
+
+
+        public DateTime DateCreate{set; get; }
+
+        public string SeoAlias{set; get; }
+
+    }
+}
